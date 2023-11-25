b9aee607e7baf20e086907accfa4b4aaf0a870f2
chidubem34 <kizito65623281@gmail.com>
2023-11-25 18:29:27 +0200

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,9 +60,9 @@ const Navbar = () => {
                             </div>
                             <div className="hidden sm:ml-6 sm:block">
                                 <div className="flex space-x-4">
-                                    <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-                                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
-                                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
+                                    <Link to={"/chidubem-kizito/"} className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</Link>
+                                    <Link to={"/chidubem-kizito/about"} className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</Link>
+                                    <Link to={"/chidubem-kizito/projects"} className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</Link>
                                 </div>
                             </div>
                         </div>
